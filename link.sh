bbf1d440dc9a744140d0e42d2d848668eaa80e08
Nathan Seither <nathanseither@gmail.com>
2021-12-09 14:36:35 -0800

    Track two Brewfiles
    Supply the desired Brewfile to link at the start. Defaults to minimal.
    
    This is a janky way to handle it and another tool probably does it
    better, but this is good enough for now.

--- link.sh
+++ link.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+BREWFILE=${BREWFILE:-Brewfile.minimal}
 DIR=$(cd $(dirname $0) && pwd -P)
 EXCLUDE_STRING=`cat nonlinkers.txt | sed -e "s/\(.*\)/-not -name \1/" | tr "\n" " "`
+
+if [ ! -e $PWD/$BREWFILE ];
+then
+  echo "Brewfile does not exist. Aborting."
+  exit 1
+fi
 
 DOTFILES=`find $DIR \
   $EXCLUDE_STRING \
   -mindepth 1 \
   -maxdepth 1 \
   2>/dev/null`
+
+# Handle Brewfile separately
+brewfile_source=$PWD/$BREWFILE
+brewfile_target=$HOME/`basename Brewfile`
+
+rm -f $brewfile_target
+ln -s $brewile_source $brewfile_target
 
 for file in $DOTFILES; do
   destination=$HOME/`basename $file`
